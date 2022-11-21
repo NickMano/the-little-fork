1c43e4da2d1974f6c0c3f6bab691dc31af3ff9b5
Nico <nmanograsso@gmail.com>
2022-11-21 13:43:32 -0300

    remove: unused images

--- TheLittleFork/Utils/Images.swift
+++ TheLittleFork/Utils/Images.swift
@@ -8,19 +8,9 @@
 import UIKit
 
 extension UIImage {
-    static let bubblesEmpty = UIImage(named: "bubblesEmpty") ?? UIImage()
-    static let bubblesHalf = UIImage(named: "bubblesHalf") ?? UIImage()
-    static let bubblesFull = UIImage(named: "bubblesFull") ?? UIImage()
-
     static let heartEmpty = UIImage(named: "emptyHeart") ?? UIImage()
     static let heartFull = UIImage(named: "filledHeart") ?? UIImage()
-    static let heartSolid = UIImage(named: "solidHeart") ?? UIImage()
 
-    static let cash = UIImage(named: "cash") ?? UIImage()
-    static let food = UIImage(named: "food") ?? UIImage()
-    static let location = UIImage(named: "location") ?? UIImage()
     static let logo = UIImage(named: "logo") ?? UIImage()
-    static let share = UIImage(named: "share") ?? UIImage()
-    static let sort = UIImage(named: "sort") ?? UIImage()
     static let tripLogo = UIImage(named: "tripLogo") ?? UIImage()
 }
